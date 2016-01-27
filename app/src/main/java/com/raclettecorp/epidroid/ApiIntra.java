eebb3c372a6b528e2b548890523710486fb0f536
villai_a <villai_a@epitech.net>
2016-01-27 18:47:25 +0100

    ApiIntra

--- app/src/main/java/com/raclettecorp/epidroid/ApiIntra.java
+++ app/src/main/java/com/raclettecorp/epidroid/ApiIntra.java
@@ -1,7 +1,39 @@
 package com.raclettecorp.epidroid;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 /**
- * Created by Théophile on 27/01/2016.
+ * Class for managing requests to intra
  */
-public class ApiIntra {
+public class ApiIntra
+{
+    String token;
+    String mLogin;
+    String mPassword;
+    public boolean connectIntra(Context context)
+    {
+        try
+        {
+            URL obj = new URL(context.getString(R.string.http_login));
+
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+            con.setRequestMethod(context.getString(R.string.request_post));
+            con.setRequestProperty(context.getString(R.string.string_login), mLogin);
+            con.setRequestProperty(context.getString(R.string.string_password), mPassword);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.d(context.getString(R.string.app_name), e.toString());
+            return false;
+        }
+    }
+
 }
